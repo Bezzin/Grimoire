6f054f2bff4bfd3d3a253f4493e340fbea8a28ac
Nathaniel <nathanieltb@live.co.uk>
2026-03-01 18:10:13 +0000

    feat: add social OAuth platform config constants

--- packages/shared/constants/index.ts
+++ packages/shared/constants/index.ts
@@ -1,6 +1,10 @@
 export { PLANS, type PlanKey } from "./plans"
 export { SOCIAL_PLATFORMS, type SocialPlatformKey } from "./platforms"
 export {
+  SOCIAL_OAUTH_CONFIG,
+  type SocialOAuthPlatform,
+} from "./social-oauth"
+export {
   TEMPLATES,
   getTemplateById,
   getTemplatesByCategory,
