ff1f1ff987b5e039300c8ca2176a149bd50eeca7
Nathaniel <nathanieltb@live.co.uk>
2026-03-01 17:54:21 +0000

    feat: add brand asset tRPC router with CRUD and plan limits

--- packages/api/root.ts
+++ packages/api/root.ts
@@ -2,12 +2,14 @@ import { createTRPCRouter } from "./trpc"
 import { userRouter } from "./routers/user"
 import { billingRouter } from "./routers/billing"
 import { brandRouter } from "./routers/brand"
+import { brandAssetRouter } from "./routers/brandAsset"
 import { contentRouter } from "./routers/content"
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
   billing: billingRouter,
   brand: brandRouter,
+  brandAsset: brandAssetRouter,
   content: contentRouter,
 })
 
